9c58794cd413c2613f7bb62f8a886201088bcac8
IgorSmirnov <smirnoviv@rambler.ru>
2013-12-26 19:42:41 +0600

    Created engine.js

---
+++ engine.js
@@ -0,0 +1,17 @@
+var Engine = 
+{
+	Run = function()
+	{
+
+	},
+	OnInit = function()
+	{
+    	CMenu.Add({
+    		run:{label: "Исполнение",
+    			run:{label: "Запустить", click: this.run}
+    		}
+	}
+}
+
+
+Main.Modules.push(Engine);
